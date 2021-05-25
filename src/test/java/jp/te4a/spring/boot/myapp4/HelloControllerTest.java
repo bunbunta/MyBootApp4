fffb907ffb0257869f528d32d567d2c4610a9797
Bunta Nakajima <83932748+bunbunta@users.noreply.github.com>
2021-05-25 11:30:31 +0900

    Revert "kadai5"

--- src/test/java/jp/te4a/spring/boot/myapp4/HelloControllerTest.java
+++ src/test/java/jp/te4a/spring/boot/myapp4/HelloControllerTest.java
@@ -1,11 +1,8 @@
 package jp.te4a.spring.boot.myapp4;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 
 import org.junit.jupiter.api.Test;
-
-import jp.te4a.spring.boot.myapp5.HelloController;
 
 public class HelloControllerTest {
 
@@ -13,7 +10,7 @@ public class HelloControllerTest {
 	void test() {
 		HelloController helloController = new HelloController();
 		String expect = "this is Spring Boot sample.";
-		String actual = "this is Spring Boot sample.";
+		String actual = helloController.index();
 		
 		assertEquals(expect,actual);
 	}
